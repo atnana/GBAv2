5ab6a6613faa7669810e09abc15654ff7b9615aa
atnana <92773909+atnana@users.noreply.github.com>
2022-07-13 17:39:46 +0800

    Add files via upload

---
+++ js/cityu.config.js
@@ -0,0 +1,15 @@
+/*
+ *
+ * City University of Hong Kong
+ * ----------------------------
+ * Site Config
+ *
+ */
+
+'use strict';
+
+var cityuSiteConfig = {
+    mode: 'auto', // auto|prod|dev
+    aos: 'cityu', // cityu|base|none
+    logoutLink: 'on', // on|off
+};
